e9977185413d927ad4ff72bb02935ec4f9dcd686
ArijitKrPaul <arijitkmrpl46@gmail.com>
2026-05-28 14:05:25 +0530

    emp dashboard changes

--- src/components/empDashboard.jsx
+++ src/components/empDashboard.jsx
@@ -7,7 +7,7 @@ import Paper from "@mui/material/Paper";
 import Popper from "@mui/material/Popper";
 import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // import { useNavigate } from "react-router-dom";
 import DashboardCard from "./DashboradCard";
